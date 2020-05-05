59c077f45a92c2e92ff3c13e4a41cde81f605c2d
Dozent <tasks.sorokin@gmail.com>
2020-05-05 10:15:40 +0300

    Push project

---
+++ src/index.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import { StatusBar } from 'react-native'
+import Amplify from '@aws-amplify/core'
+import { Authenticator } from 'aws-amplify-react-native'
+import { AmplifyTheme, Localei18n } from './components'
+import awsconfig from '../aws-exports'
+
+
+Amplify.configure({
+  ...awsconfig,
+  Analytics: {
+    disabled: true,
+  },
+})
+
+const signUpConfig = {
+  hideAllDefaults: true,
+  signUpFields: [
+    {
+      label: 'Name',
+      key: 'name',
+      required: true,
+      displayOrder: 1,
+      type: 'string',
+    },
+    {
+      label: 'Email',
+      key: 'email',
+      required: true,
+      displayOrder: 1,
+      type: 'string',
+    },
+    {
+      label: 'Password',
+      key: 'password',
+      required: true,
+      displayOrder: 2,
+      type: 'password',
+    },
+  ],
+}
+
+handleAuthStateChange(state) {
+  if (state === 'signedIn') {
+    /* Do something when the user has signed-in */
+  }
+}
+
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <Localei18n />
+      <Authenticator 
+        hideDefault={true} 
+        onStateChange={this.handleAuthStateChange}
+        usernameAttributes="email" 
+        signUpConfig={signUpConfig} 
+        theme={AmplifyTheme} />
+    </>
+  )
+}
+
+export default App
+
+
